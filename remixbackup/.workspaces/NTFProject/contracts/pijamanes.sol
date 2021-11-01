135fb57307bcfd045d815d6a536e12c076147ab4
napodevesa <napodevesa@gmail.com>
2021-10-31 23:39:18 -0300

    1

---
+++ remixbackup/.workspaces/NTFProject/contracts/pijamanes.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+
+//import ERC 1155 token contract from OppenZepellin
+//SPDX-License-Identifier: UNLICENSED
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+
+contract NTFProject is ERC1155, Ownable {
+    
+    uint256 public constant ARTWORK = 0;
+    uint256 public constant PHOTO = 1;
+    
+    constructor() ERC1155 ("https://e3hgdmhctnx6.usemoralis.com/{id}.json") {
+        _mint(msg.sender, ARTWORK, 0, "");
+        _mint(msg.sender, PHOTO , 1, "");
+    }
+    
+    function mint ( address account, uint256 id, uint256 amount) public onlyOwner{
+        _mint(account,id,amount,"");
+    }
+    
+    function burn (address account, uint256 id, uint256 amount ) public{
+        require (msg.sender == account);
+        _burn (account, id, amount );
+    }
+    
+}
